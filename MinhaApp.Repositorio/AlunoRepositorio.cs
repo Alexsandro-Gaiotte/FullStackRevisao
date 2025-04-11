258a46903674d016e63b55aa836dd6ae042b5c45
Alexsandro <alex_gaiotte@hotmail.com>
2025-04-11 14:23:36 -0300

    Tudo Incluso

---
+++ MinhaApp.Repositorio/AlunoRepositorio.cs
@@ -0,0 +1,27 @@
+﻿using MinhaApp.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaApp.Repositorio
+{
+    public class AlunoRepositorio : IAlunoRepositorio
+    {
+        private readonly AppDbContext _context;
+        public AlunoRepositorio(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void Salvar(Aluno aluno)
+        {
+            _context.Alunos.Add(aluno);
+            _context.SaveChanges();
+        }
+        public IEnumerable<Aluno> Listar()
+        {
+            return _context.Alunos.ToList();
+        }
+    }
+}
---
+++ MinhaApp.Repositorio/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinhaApp.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaApp.Repositorio
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Aluno> Alunos { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options){ }
+    }
+}
